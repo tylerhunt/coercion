82871be50c8b4cbf092968ab35e825814f599d58
Tyler Hunt <tyler@tylerhunt.com>
2008-10-01 14:50:22 -0400

    Added a method to allow serialized columns to be coereced.
    Fixed that text columns weren't being coerced to nil.

--- lib/coercion.rb
+++ lib/coercion.rb
@@ -1,8 +1,24 @@
 module Coercion
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
   def coerce_blanks_to_nils
     self.class.columns.each do |column|
-      if column.type == :string
-        write_attribute(column.name, nil) if read_attribute(column.name).blank?
+      if column.text? && read_attribute(column.name).blank?
+        write_attribute(column.name, nil)
+      end
+    end
+  end
+
+  module ClassMethods
+    def coerce_serialized_attributes
+      serialized_attributes.each do |column_name, type|
+        module_eval <<-DEF, __FILE__, __LINE__ + 1 if type == Symbol
+          def #{column_name}=(value)
+            write_attribute(:#{column_name}, value ? value.to_sym : value)
+          end
+        DEF
       end
     end
   end
